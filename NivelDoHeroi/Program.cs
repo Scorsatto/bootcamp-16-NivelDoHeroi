26d2218373a72f77b294cf2538102ef4e7d5d09b
Scorsatto <Scorsatto@SCORSATTO-PC>
2024-12-09 13:46:55 -0400

    Adicionar arquivos de projeto.

---
+++ NivelDoHeroi/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Informe o nome do Heroi");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Informe a quantidade de experiëncia");
+int experiencia = Convert.ToInt32(Console.ReadLine());
+
+string nivel = "";
+
+switch (experiencia)
+{
+    case <= 1000:
+        nivel = "Ferro";
+        break;
+
+    case <= 2000:
+        nivel = "Bronze";
+        break;
+
+    case <= 5000:
+        nivel = "Prata";
+        break;
+
+    case <= 7000:
+        nivel = "Ouro";
+        break;
+
+    case <= 8000:
+        nivel = "Platina";
+        break;
+
+    case <= 9000:
+        nivel = "Ascendente";
+        break;
+
+    case <= 10000:
+        nivel = "Imortal";
+        break;
+
+    case > 10000:
+        nivel = "Radiante";
+        break;
+}
+
+Console.WriteLine($"O Herói de nome {nome} está no nível de {nivel}");
